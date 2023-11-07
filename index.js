97977393d0740b164bb27e4a478cf4a8de927a71
Ana Rosa <dosanarosa03@gmail.com>
2023-11-07 20:44:25 -0300

    tô lascada

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ function changeTitle(title = '') {
 
 // Programa que executa uma busca.
 function search() {
-    var query = stripTags($("input[name='q']").val()); // Obtém e filtra o termoa ser buscado.
+    var query = stripTags($("input[name='q']").val()); // Obtém e filtra o termo a ser buscado.
     console.log(query); // Exibe no console. Será substituído pelo processamento do programa.
     return false; // Sai do programa sem fazer mais nada.
 }
